3029f0256d5edc0d89dc8468fd12776cf7e7bed2
aguss rodriguez <agussrg80@gmail.com>
2022-03-27 01:41:24 -0300

    add nav

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,18 @@
+import './NavBar.css'
+
+import Navegacion from '../Navegacion/Navegacion'
+
+const NavBar = () => {
+    return(
+        <nav>
+            <div>
+                <a href="# ">DiTech</a>
+            </div>
+            <div>
+                <Navegacion/>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
--- src/index.js
+++ src/index.js
@@ -2,16 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+// El index.js es en donde tengo conectada toda mi aplicacion con el DOM real  
+ReactDOM.render(<App />, document.getElementById('root'));
+// el componente que 'imprime' es el app por ende tengo que llamar a todos mis componentes en App.s
+
